f32f0ed7767279b0f2355dba63679f3ce7cdee7b
JasonLiu1229 <100850638+JasonLiu1229@users.noreply.github.com>
2024-03-20 17:44:46 +0100

    seperated scripts and temporary scripts

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-import uvicorn
 import argparse
 
-from app import app
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Country API")
+parser = argparse.ArgumentParser(description="Country API")
 
-    parser.add_argument("-key", "--openweathermap-api-key",
-                        required=True,
-                        help="OpenWeatherMap API key",
-                        type=str
-                        )
+parser.add_argument("-key", "--openweathermap-api-key",
+                    required=True,
+                    help="OpenWeatherMap API key",
+                    type=str
+                    )
 
-    # write keys to a file
-    with open('keys.txt', 'w') as file:
-        file.write(parser.parse_args().openweathermap_api_key)
-
-    uvicorn.run(app, port=8000)
+# write keys to a file
+with open('keys.txt', 'w') as file:
+    file.write(parser.parse_args().openweathermap_api_key)
