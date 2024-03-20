fec0f409a9f7089fc043fc909a43e40c7a50a1a6
JasonLiu1229 <100850638+JasonLiu1229@users.noreply.github.com>
2024-03-20 14:47:11 +0100

    favorites done

--- routers/favorite.py
+++ routers/favorite.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException
+import httpx
 
 router = APIRouter(
     prefix="/favorite",
@@ -7,6 +8,7 @@ router = APIRouter(
 )
 
 FAVORITE_FILE = 'favorite.txt'
+
 
 @router.get("")
 async def get_favorite():
@@ -30,7 +32,19 @@ async def add_favorite(country: str):
     :param country: country in string
     :return: message in json response
     """
-    raise HTTPException(status_code=501, detail="Not implemented")
+    # make a request to rest countries to check if country exists
+    url = f"https://restcountries.com/v3.1/name/{country}"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        if response.status_code != 200:
+            raise HTTPException(status_code=404, detail="Country not found")
+    # add country to a favorite list
+    try:
+        with open(FAVORITE_FILE, 'a') as file:
+            file.write(country + '\n')
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="Favorite list not found")
+    return {"message": f"{country} added to favorite list"}, 200
 
 
 @router.delete("/{country}")
@@ -40,4 +54,15 @@ async def remove_favorite(country: str):
     :param country: country in string
     :return: message in json response
     """
-    raise HTTPException(status_code=501, detail="Not implemented")
+    try:
+        with open(FAVORITE_FILE, 'r') as file:
+            favorite_list = file.read().splitlines()
+        if country not in favorite_list:
+            raise HTTPException(status_code=404, detail="Country not found in favorite list")
+        with open(FAVORITE_FILE, 'w') as file:
+            for item in favorite_list:
+                if item != country:
+                    file.write(item + '\n')
+        return {"message": f"{country} removed from favorite list"}, 200
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="Favorite list not found")
