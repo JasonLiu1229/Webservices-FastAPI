0b557aacf0ed3a883537de1d72be5a2f3ca3c710
JasonLiu1229 <100850638+JasonLiu1229@users.noreply.github.com>
2024-03-20 17:11:00 +0100

    last function done

--- routers/country.py
+++ routers/country.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, HTTPException
 import httpx
+import datetime
+import time
 
 router = APIRouter(
     prefix="/country",
@@ -33,7 +35,7 @@ async def get_country(continent: str):
         # filter out the country names
         country_list = [country['name']['common'] for country in countries]
         # return list of countries as json response
-        return {"content": country_list}, 200
+        return {"content": country_list}
 
 
 @router.get("/{country}")
@@ -53,7 +55,15 @@ async def get_country_detail(country: str):
             raise HTTPException(status_code=404, detail=COUNTRY_ERROR)
         country_detail = response.json()
         # return details of the country as json response
-        return {"content": country_detail}, 200
+        # retrieve longitude and latitude, population and area of capital
+        country_detail_filtered = {
+            "capital": country_detail[0]['capital'],
+            "latitude": country_detail[0]['capitalInfo']['latlng'][0],
+            "longitude": country_detail[0]['capitalInfo']['latlng'][1],
+            "population": country_detail[0]['population'],
+            "area": country_detail[0]['area']
+        }
+        return {"content": country_detail_filtered}
 
 
 @router.get("/{country}/temperature")
@@ -73,18 +83,8 @@ async def get_country_temperature(country: str):
         if response.status_code != 200:
             raise HTTPException(status_code=404, detail=COUNTRY_ERROR)
         country_detail = response.json()
-        capital = country_detail[0]['capital']
-
-    # get capital latitude and longitude
-    url = f"https://restcountries.com/v3.1/capital/{capital}"
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-        if response.status_code != 200:
-            raise HTTPException(status_code=404, detail="Capital not found")
-        capital_detail = response.json()
-        latlng = capital_detail[0]['latlng']
-        latitude = latlng[0]
-        longitude = latlng[1]
+        latitude = country_detail[0]['capitalInfo']['latlng'][0]
+        longitude = country_detail[0]['capitalInfo']['latlng'][1]
 
     # retrieve the keys from the keys.txt
     with open('keys.txt', 'r') as file:
@@ -107,10 +107,14 @@ async def get_country_temperature(country: str):
 async def get_country_forecast(country: str, day: int):
     """
     Get forecast of a country.
+    Three-hourly based on the current date.
     :param country: country in string
     :param day: day in integer within a boundary of 1 to 5
     :return: forecast of the country in json response
     """
+    if day < 1 or day > 5:
+        raise HTTPException(status_code=400, detail="Day should be within 1 to 5")
+
     country = country.lower()
     day = int(day)
 
@@ -118,4 +122,79 @@ async def get_country_forecast(country: str, day: int):
     # from data provided from openweathermap
 
     # make a request to rest countries to get the capital of a country
+    url = f"https://restcountries.com/v3.1/name/{country}"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        if response.status_code != 200:
+            raise HTTPException(status_code=404, detail=COUNTRY_ERROR)
+        country_detail = response.json()
+        latitude = country_detail[0]['latlng'][0]
+        longitude = country_detail[0]['latlng'][1]
 
+    # retrieve the keys from the keys.txt
+    with open('keys.txt', 'r') as file:
+        keys = file.read().splitlines()
+        # retrieve the key from the keys
+        key = keys[0]
+
+    current_date = datetime.datetime.now()
+
+    saved_data = []
+
+    # make a request to openweathermap to get the forecast of the capital
+    url = (
+        f"https://api.openweathermap.org/data/2.5/forecast?"
+        f"lat={latitude}"
+        f"&lon={longitude}"
+        f"&appid={key}"
+        f"&units=metric"
+        f"&cnt={day * 8}"
+    )
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        if response.status_code != 200:
+            raise HTTPException(status_code=404, detail="Forecast not found")
+        forecast = response.json()
+        # return forecast of the country as json response
+
+    # make a request to QuickChart to generate a chart
+    url = "https://quickchart.io/chart/create"
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, json=convert_data_to_chartJS(forecast.get('list')))
+        if response.status_code != 200:
+            raise HTTPException(status_code=404, detail="Chart not found")
+        chart = response.json()
+        # return chart of the forecast as json response
+        return {"content": chart['url']}, 200
+
+
+
+
+def convert_data_to_chartJS(data: list):
+    """
+    Convert a list of data to chartJS format supported by QuickChart
+
+    :param data: List of data
+    :return: chartJS format
+    """
+    labels = []
+    temperatures = []
+    chart_data = {}
+    for item in data:
+        # convert date to human readable format
+        date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(item['dt']))
+        labels.append(date)
+        temperatures.append(item.get('main').get('temp'))
+    chart_data['labels'] = labels
+    chart_data['datasets'] = [
+        {
+            "label": "Temperature",
+            "data": temperatures,
+            "fill": False,
+            "borderColor": "rgb(75, 192, 192)",
+            "lineTension": 0.1
+        }
+    ]
+    return chart_data
