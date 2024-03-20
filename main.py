c0bba17b5e938145f0d5cb7c965c3d13137fade1
JasonLiu1229 <100850638+JasonLiu1229@users.noreply.github.com>
2024-03-20 14:41:23 +0100

    arg parse and favorites further implemented

--- main.py
+++ main.py
@@ -3,7 +3,24 @@ import argparse
 
 from app import app
 
-
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Country API")
+
+    parser.add_argument("-owm-key", "--openweathermap-api-key",
+                        required=True,
+                        help="OpenWeatherMap API key",
+                        type=str
+                        )
+
+    parser.add_argument("-rc-key",
+                        "--restfull-countries-api-key",
+                        required=True,
+                        help="Restful Countries API key",
+                        )
+
+    # write keys to a file
+    with open('keys.txt', 'w') as file:
+        file.write(f"OWM_KEY={parser.openweathermap_api_key}\n")
+        file.write(f"RC_KEY={parser.restfull_countries_api_key}\n")
+
     uvicorn.run(app, port=8000)
--- routers/favorite.py
+++ routers/favorite.py
@@ -6,6 +6,7 @@ router = APIRouter(
     responses={404: {"description": "Not found"}}
 )
 
+FAVORITE_FILE = 'favorite.txt'
 
 @router.get("")
 async def get_favorite():
@@ -13,7 +14,13 @@ async def get_favorite():
     Get a list of favorite items.
     :return: list of favorite items in json response
     """
-    raise HTTPException(status_code=501, detail="Not implemented")
+    try:
+        with open(FAVORITE_FILE, 'r') as file:
+            favorite_list = file.read().splitlines()
+            # return list as json response
+            return {"content": favorite_list}, 200
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="Favorite list not found")
 
 
 @router.post("/{country}")
